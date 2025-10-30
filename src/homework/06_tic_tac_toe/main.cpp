d03161ac50910551ab8866dfb9393ee69f236be0
Tyler Lane <144444269+Tylerdlane@users.noreply.github.com>
2025-10-30 23:02:44 +0000

    tictactoe

--- src/homework/06_tic_tac_toe/main.cpp
+++ src/homework/06_tic_tac_toe/main.cpp
@@ -1,6 +1,24 @@
+#include "tic_tac_toe.h"
+
+using std::string;
+using std::cin;
+using std::cout;
+
 
 int main() 
 {
+	TicTacToe tic_tac_toe;
 
+	string first_player ="X";
+
+	cout<<"Enter First player (X or O)";
+	cin>>first_player;
+
+
+	// while(!tic_tac_toe.game_over())
+	// {
+	// 	auto positoin = 0;
+	// 	cout<<"Enter position: "
+	// }
 	return 0;
 }
