98940d103a621c6a6ba04eca00e72cf7bb636fbc
Tyler Lane <144444269+Tylerdlane@users.noreply.github.com>
2025-11-10 23:46:04 +0000

    HW07-late

--- src/homework/06_tic_tac_toe/tic_tac_toe.h
+++ src/homework/06_tic_tac_toe/tic_tac_toe.h
@@ -1,25 +1,35 @@
-#include <string>
-#include <vector>
-#include <iostream>
-
 #ifndef TIC_TAC_TOE_H
 #define TIC_TAC_TOE_H
 
+#include <iostream>
+#include <string>
+#include <vector>
+
+using std::string;
+using std::vector;
+
 class TicTacToe
 {
-    public:
-        bool game_over();
-        void start_game(std::string first_player);
-        void mark_board(int position);
-        std::string get_player() const {return player;}
-        void display_board()const;
-    private:
-    std::string player;
-    std::vector<std::string> pegs{9, " "};
-    void set_next_player();
-    bool check_board_full();
-    void clear_board();
+public:
+    void start_game(string first_player);
+    void mark_board(int position);
+    void display_board() const;
+    bool game_over();
+    string get_player() const { return player; }
+    string get_winner() const { return winner; }
 
+private:
+    string player;
+    string winner;
+    vector<string> pegs{9, " "};
+
+    void set_next_player();
+    bool check_board_full() const;
+    void clear_board();
+    void set_winner();
+    bool check_column_win() const;
+    bool check_row_win() const;
+    bool check_diagonal_win() const;
 };
 
 #endif
--- test/homework/06_tic_tac_toe/tic_tac_toe_tests.cpp
+++ test/homework/06_tic_tac_toe/tic_tac_toe_tests.cpp
@@ -1,8 +1,59 @@
-#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#define CATCH_CONFIG_MAIN
 #include "catch.hpp"
 #include "tic_tac_toe.h"
 
-TEST_CASE("Verify Test Configuration", "verification") {
-	REQUIRE(true == true);
+TEST_CASE("Test first player set to X")
+{
+    TicTacToe game;
+    game.start_game("X");
+    REQUIRE(game.get_player() == "X");
 }
 
+TEST_CASE("Test first player set to O")
+{
+    TicTacToe game;
+    game.start_game("O");
+    REQUIRE(game.get_player() == "O");
+}
+
+TEST_CASE("Test tie game")
+{
+    TicTacToe game;
+    game.start_game("X");
+    game.mark_board(1);
+    game.mark_board(2);
+    game.mark_board(3);
+    game.mark_board(5);
+    game.mark_board(4);
+    game.mark_board(6);
+    game.mark_board(8);
+    game.mark_board(7);
+    game.mark_board(9);
+
+    REQUIRE(game.game_over() == true);
+    REQUIRE(game.get_winner() == "C");
+}
+
+TEST_CASE("Test win by first column")
+{
+    TicTacToe game;
+    game.start_game("X");
+    game.mark_board(1);
+    game.mark_board(2);
+    game.mark_board(4);
+    game.mark_board(5);
+    game.mark_board(7);
+    REQUIRE(game.game_over() == true);
+}
+
+TEST_CASE("Test win diagonally from top left")
+{
+    TicTacToe game;
+    game.start_game("X");
+    game.mark_board(1);
+    game.mark_board(2);
+    game.mark_board(5);
+    game.mark_board(3);
+    game.mark_board(9);
+    REQUIRE(game.game_over() == true);
+}
